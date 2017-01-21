4b4af030da2c0f1cc7ca86ed6c9162ef93b7a2e6
Michael Litchard <michael@schmong.org>
2017-01-21 09:03:28 -0800

    fixed evaluation bug

--- src/ColorGuess.hs
+++ src/ColorGuess.hs
@@ -1,13 +1,14 @@
-{-# LANGUAGE GADTs #-}
 module ColorGuess (runGameM_) where
 
 
 import Control.Monad.State (StateT,runStateT)
 import System.Random (newStdGen)
-import Control.Monad.Prompt (Prompt,runPromptM)
+import Control.Monad.Prompt (Prompt,runPromptM,prompt)
+import Data.Monoid ((<>))
 
 import Types
 import Utilities
+import Interpreter
 
 runGameM_ :: IO ()
 runGameM_ = runGameM >> return ()
@@ -20,14 +21,26 @@ play :: Prompt GuessPrompt ()
 play = do
   action <- queryPrompt
   case action of
-    (ColorIs color) -> guess color >> play 
-    Hint          -> hint >> play
+    (ColorIs color) -> guess color  
+    Hint          -> hint
     NOOP          -> play
     End           -> end
 
-gameInterp :: GuessPrompt a -> StateT (Secret Color) IO a
-gameInterp (Say str)     = gPrint str
-gameInterp (Query str)   = getGuess str
-gameInterp (Guess color) = evalGuess color
-gameInterp Quit          = return ()
+queryPrompt :: Prompt GuessPrompt Action
+queryPrompt = prompt (Say commands) >> prompt (Query guess')
+  where
+    guess'    = "Make a guess. Colors are " <> (concatMap show colors)
+    commands = "Commands are <ColorIs color> <Hint> <End>"
 
+guess :: Color -> Prompt GuessPrompt ()
+guess color = prompt (Guess color) >>= fromResult
+
+hint :: Prompt GuessPrompt ()
+hint = prompt (Say "a hint, really?") >> play
+
+end :: Prompt GuessPrompt ()
+end = prompt Quit
+
+fromResult :: Result -> Prompt GuessPrompt ()
+fromResult Correct = prompt (Say "you are correct sir!") >> end
+fromResult res     = prompt (Say (show res)) >> play
--- src/Utilities.hs
+++ src/Utilities.hs
@@ -1,50 +1,10 @@
 module Utilities 
-  ( pickColor
-  , colors
-  , queryPrompt
-  , guess
-  , hint
-  , end
-  , evalGuess
-  , gPrint
-  , getGuess 
-  ) where
+  ( pickColor, colors ) where
 
-import Safe (readDef)
 import System.Random (RandomGen)
 import System.Random.Shuffle (shuffle')
-import Control.Monad.State (StateT,lift,get)
 
-import Lens.Micro ((<&>))
-import Control.Monad.Prompt (Prompt,prompt)
-import Data.Monoid ((<>))
-import Types
-
--- | Prompts
-queryPrompt :: Prompt GuessPrompt Action
-queryPrompt = prompt (Say commands) >> prompt (Query guess')
-  where
-    guess'    = "Make a guess. Colors are " <> (concatMap show colors)
-    commands = "Commands are <ColorIs color> <Hint> <End>"
-
-guess :: Color -> Prompt GuessPrompt ()
-guess color = prompt (Guess color) >>= fromResult
-
-hint :: Prompt GuessPrompt ()
-hint = prompt (Say "a hint, really?") 
-
-end :: Prompt GuessPrompt () 
-end = prompt Quit
-
--- | For the interpreter
-evalGuess :: Color -> StateT (Secret Color) IO Result
-evalGuess color = toEnum <$> fromEnum <$> compare color <$> fromSecret <$> get
-
-gPrint :: String -> StateT (Secret Color) IO ()
-gPrint str = lift (putStrLn str)
-
-getGuess :: String -> StateT (Secret Color) IO Action
-getGuess str = lift (putStrLn str) >> lift getLine <&> readDef NOOP
+import Types (Secret (..), Color (..))
 
 
 -- | Utilities for color choosing
@@ -54,10 +14,3 @@ pickColor gen = Secret color
 
 colors :: [Color]
 colors = [Red .. Blue]
-
--- | Internal
-fromResult :: Result -> Prompt GuessPrompt ()
-fromResult Correct = prompt (Say "you are correct sir!") >> end
-fromResult res     = prompt (Say (show res)) 
-
-
